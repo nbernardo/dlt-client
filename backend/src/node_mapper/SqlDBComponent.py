99a705254c5b31f8cdce315e17e547fa6ea52da0
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-05-10 22:07:52 -0700

    - initial implementation of the SQLDBComponent in the Backend for allowing parsing of Source SQL Database

---
+++ backend/src/node_mapper/SqlDBComponent.py
@@ -0,0 +1,60 @@
+from .TemplateNodeType import TemplateNodeType
+from controller.RequestContext import RequestContext
+from connectors.db.mysql import get_mysql_connection
+
+
+class SqlDBComponent(TemplateNodeType):
+    """
+    Bucket type mapping class
+    """
+
+    def __init__(self, data: dict, context: RequestContext):
+        """
+        Initialize the instance
+        """
+        self.context = context
+        self.tables = data['tables']
+        self.database = data['database']
+        self.component_id = data['componentId']
+
+        self.context.emit_start(self, '')
+
+
+
+    def run(self) -> None:
+        """
+        Run the initial steps
+        """
+        super().run()
+        print(f'Inited Source SqlDb with : {self.database} and {self.tables}')
+        print(f'ITS LIST: {self.tables.values()}')
+        self.check_table(list(self.tables.values()))
+        #self.check_table()
+
+    def check_db():
+        """
+        Check if the DB exists
+        """
+
+
+    def check_table(self, tables: list[str]) -> None:
+        """
+        Check if the table already exists
+        """
+        query_template = 'SELECT 1 FROM @tblName'
+        final_query = query_template.replace('@tblName', tables[0])
+        try:
+
+            for tbl in tables[1:]:
+                final_query += ' UNION '
+                final_query += query_template.replace('@tblName', tbl)
+
+            #If specified DB does not exists it'll throw an exception
+            db_connection = get_mysql_connection(self.database)
+            #If any of tables in the query does not exists it'll throw an exception
+            db_connection.cursor().execute(final_query)
+
+        except Exception as err:
+            error = {'message': f'{err}', 'componentId': self.component_id}
+            self.context.emit_error(self, error)
+
