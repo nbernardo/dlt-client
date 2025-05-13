492e2438e0eb7dd6efaeb7fdaa252dce4f598dcf
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-05-13 10:43:25 -0700

    - adjustments on the backend workspace service to run Python code submission - implementation of the running result handling in the terminal - implemented frontend runcode in the workspace service

--- backend/src/services/workspace/Workspace.py
+++ backend/src/services/workspace/Workspace.py
@@ -1,11 +1,13 @@
 import subprocess
 from pathlib import Path
 import uuid
+import os
 
 class Workspace:
     
     @staticmethod
     def run_code(payload):
+        # create a fiile to hold the submitted code to be ran
         file_path = f'{Workspace.get_code_path()}/code_{uuid.uuid4()}.py'
         with open(file_path, 'x+') as _file:
             _file.write(payload)
@@ -16,10 +18,16 @@ class Workspace:
                                 capture_output=True,
                                 text=True)
             
-            final_result = result.stdout if result.stderr == '' or result.stderr == 0 else result.stderr
+            final_result = result.stdout \
+                if result.stderr == '' or result.stderr == 0 \
+                else result.stderr
             
         except subprocess.CalledProcessError as err:
             final_result = err.stderr
+
+        if os.path.exists(file_path):
+            #Remove the file after the code runs
+            Workspace.remove_code_file(file_path)
 
         return final_result
         
@@ -40,4 +48,8 @@ class Workspace:
     def get_ppline_path():
         root_dir = str(Path(__file__).parent).replace('src/services/workspace','destinations')
         return f'{root_dir}/pipeline'
+    
+    @staticmethod
+    def remove_code_file(file):
+        os.remove(file)
 
