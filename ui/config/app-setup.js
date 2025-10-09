8a2786da4102969281e8a68c3a04076255397515
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-10-09 18:42:43 -0400

    small adjustments on the component prefetch

--- ui/config/app-setup.js
+++ ui/config/app-setup.js
@@ -1,5 +1,4 @@
 import { StillAppMixin } from "../@still/component/super/AppMixin.js";
-import { StillHTTPClient } from "../@still/helper/http.js";
 import { Components } from "../@still/setup/components.js";
 import { AppTemplate } from "./app-template.js";
 import { Workspace } from "../app/components/workspace/Workspace.js";
@@ -18,27 +17,19 @@ export class StillAppSetup extends StillAppMixin(Components) {
             this.localEnv();
         }
 
-        /* this.addPrefetch({
-            component: '@codemirror/CodeMiror',
-            assets: [
-                "v5.65.19/lib/codemirror.js",
-                "v5.65.19/mode/python/python.js",
-                "v5.65.19/mode/sql/sql.js",
-                "v5.65.19/lib/codemirror.css",
-                "v5.65.19/theme/monokai.css"
-            ]
-        }); */
+        this.prefetchComponent();
+    }
 
+    prefetchComponent(){
         this.addPrefetch({
             component: '@treeview/StillTreeView',
             assets: ["tree-view.css"]
         });
         this.runPrefetch();
-
     }
 
     localEnv(){
-        this.setConfigFile('dev-https.json');        
+        this.setConfigFile('dev.json');        
     }
 
     cloudEnv(){ /* Will use the default.json condigurations file */ }
