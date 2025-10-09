a081d9740ac41fd80cc7f0e64eb3fcc9f5bc8b61
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-10-09 19:02:02 -0400

    adjustments on the app-setup and environment settings

--- ui/config/app-setup.js
+++ ui/config/app-setup.js
@@ -1,5 +1,4 @@
 import { StillAppMixin } from "../@still/component/super/AppMixin.js";
-import { StillHTTPClient } from "../@still/helper/http.js";
 import { Components } from "../@still/setup/components.js";
 import { AppTemplate } from "./app-template.js";
 import { Workspace } from "../app/components/workspace/Workspace.js";
@@ -18,33 +17,22 @@ export class StillAppSetup extends StillAppMixin(Components) {
             this.localEnv();
         }
 
-        /* this.addPrefetch({
-            component: '@codemirror/CodeMiror',
-            assets: [
-                "v5.65.19/lib/codemirror.js",
-                "v5.65.19/mode/python/python.js",
-                "v5.65.19/mode/sql/sql.js",
-                "v5.65.19/lib/codemirror.css",
-                "v5.65.19/theme/monokai.css"
-            ]
-        }); */
+        this.prefetchComponent();
+    }
 
+    prefetchComponent(){
         this.addPrefetch({
             component: '@treeview/StillTreeView',
             assets: ["tree-view.css"]
         });
         this.runPrefetch();
-
     }
 
     localEnv(){
-        StillHTTPClient.setBaseUrl('http://localhost:8000');
-        this.setConfigFile('dev.json');
+        this.setConfigFile('dev.json');        
     }
 
-    cloudEnv(){
-        StillHTTPClient.setBaseUrl('https://dlt-client.onrender.com');
-    }
+    cloudEnv(){ /* Will use the default.json condigurations file */ }
 
     async init() {
         if((await UserService.isAuthenticated()))
