2fffe07c2b218d9548027e9e4da7fedf4e8d09d5
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-11-01 00:20:31 -0400

    - adding dynamic validation for secrete creation - preparing secrete paylod on UI

--- ui/app/components/catalog/CatalogForm.js
+++ ui/app/components/catalog/CatalogForm.js
@@ -1,6 +1,8 @@
 import { ViewComponent } from "../../../@still/component/super/ViewComponent.js";
-import { State } from "../../../@still/component/type/ComponentType.js";
+import { State, STForm } from "../../../@still/component/type/ComponentType.js";
 import { FormHelper } from "../../../@still/helper/form.js";
+import { AppTemplate } from "../../../config/app-template.js";
+import { WorkspaceService } from "../../services/WorkspaceService.js";
 
 export class CatalogForm extends ViewComponent {
 
@@ -12,9 +14,20 @@ export class CatalogForm extends ViewComponent {
 	/** @Prop */ showAddSecrete = false;
 
 	/** @Prop */ dataBaseSettingType = null;
-	/** @Prop */ formRef = null;
-	/** @Prop */ formRef = null;
+	/** @Prop @type { STForm } */ formRef = null;
 	/** @Prop */ dynamicFieldCount = 0;
+	/** @Prop */ isDbFirstCall = false;
+
+	/** @type { Workspace } */ $parent;
+
+	connectionName;
+	dbEngine;
+	dbHost;
+	dbPort;
+	dbName;
+	dbUser;
+	firstKey;
+	firstValue;
 
 	stAfterInit(){
 
@@ -26,8 +39,25 @@ export class CatalogForm extends ViewComponent {
 	}
 
 	changeType(value){
-		this.showAddSecrete = true;
-		this.dataBaseSettingType = value;
+		this.showAddSecrete = true, this.dataBaseSettingType = value;
+		if(value == 1){
+			if(!this.isDbFirstCall) {
+				this.addSecreteGroup(true);
+				this.isDbFirstCall = true;
+			}
+			document.querySelectorAll('.catalog-form-db-fields input:not(.no-required), .catalog-form-db-fields select').forEach(inpt => inpt.setAttribute('required', true));
+			document.querySelectorAll('.catalog-form-secret-group input').forEach(inpt => {
+				inpt.removeAttribute('required');
+				inpt.removeAttribute('(required)');
+			});
+		}else{
+			document.querySelectorAll('.catalog-form-db-fields input, .catalog-form-db-fields select').forEach(inpt => {
+				inpt.removeAttribute('required');
+				inpt.removeAttribute('(required)');
+			});
+			document.querySelectorAll('.catalog-form-secret-group input').forEach(inpt => inpt.setAttribute('required', true));
+		}
+
 	}
 
 	showDialog(){		
@@ -41,53 +71,50 @@ export class CatalogForm extends ViewComponent {
 		const self = this;
 		//this.openModal.addEventListener('click', () => self.modal.style.display = 'flex');
 		this.closeModal.addEventListener('click', () => self.modal.style.display = 'none');
-
-		window.addEventListener('click', (e) => {
-			if (e.target === modal) self.modal.style.display = 'none';
-		});
+		window.addEventListener('click', (e) => e.target === modal ? self.modal.style.display = 'none' : '');
 	}
 
-	addSecreteGroup(){
+	addSecreteGroup(initial = false){
 		const type = this.dataBaseSettingType == 1 ? 'db' : 'secret';
 		const targetForm = type == 'db' ? 'catalog-form-db-fields' : 'catalog-form-secret-group';
-
 		this.dynamicFieldCount++;
+		const value = initial ? 'DB_PASSWORD' : '';
+		const disabled = initial ? true : false;
+		
 		let fieldName = `key${this.dynamicFieldCount}-${type}`;
-		const secretKeyField = FormHelper
-			.newField(this, this.formRef, fieldName)
-			.input({ required: true, placeholder: 'e.g. DB_PASSWORD' })
+		const secretKeyField = FormHelper.newField(this, this.formRef, fieldName)
+			.input({ required: true, placeholder: 'e.g. DB_PASSWORD', className: 'secret-field', disabled, value })
 			.element;
 
 		fieldName = `val${this.dynamicFieldCount}-${type}`;
-		const secretValField = FormHelper
-			.newField(this, this.formRef, fieldName)
+		const secretValField = FormHelper.newField(this, this.formRef, fieldName)
 			.input({ required: true, placeholder: 'Enter the secret value', type: 'password' })
 			.element;
 
-		this.addSecreteField(secretKeyField, targetForm);
-		this.addSecreteField(secretValField, targetForm, 'val', fieldName);
+		this.addSecreteField(secretKeyField, targetForm, null, fieldName);
+		this.addSecreteField(secretValField, targetForm, 'val', fieldName, initial);
 
 	}
 
-	addSecreteField = (field, targetForm, type, fieldName) => {
+	addSecreteField = (field, targetForm, type, fieldName, initial) => {
 		const div = document.createElement('div');
-		div.className = 'form-group';
-		let fieldContent = field;
+		div.className = `form-group remove-dyn-field-${fieldName}`;
 
 		if(type == 'val')
-			fieldContent = this.parseEvents(`
+			field = this.parseEvents(`
 				<span class="${fieldName} hidden-secret-value">
-					${field} <img src="app/assets/imgs/view-svgrepo-com.svg" onclick="inner.viewSecretValue('${fieldName}')">
+					${field} 
+					${!initial ? `<span onclick="inner.removeField('${fieldName}')">x</span>` : ''} 
+					<img src="app/assets/imgs/view-svgrepo-com.svg" onclick="inner.viewSecretValue('${fieldName}')">
 				<span>`);
 
-		div.innerHTML = fieldContent;
+		div.innerHTML = field;
 		document.querySelector(`.${targetForm}`).appendChild(div);
 	}
 
-	viewSecretValue(fieldContainer){
-		//view-hide-svgrepo-com
-		const type = this.dataBaseSettingType == 1 ? 'catalog-form-db-fields' : 'catalog-form-secret-group'
-		const clsName = fieldContainer == 'initial' ? '.initial-secret-field' : `.${type} .${fieldContainer}`;
+	viewSecretValue(fieldContainer, secretType){
+		const type = this.dataBaseSettingType == 1 ? 'catalog-form-db-fields' : 'catalog-form-secret-group';
+		const clsName = fieldContainer == 'initial' ? `.initial-secret-field${secretType == 'db' ? '-db' : ''}` : `.${type} .${fieldContainer}`;
 		const currentVal = document.querySelector(`${clsName} input`).type;
 		if(currentVal == 'text') {
 			document.querySelector(`${clsName} input`).type = 'password';
@@ -96,6 +123,38 @@ export class CatalogForm extends ViewComponent {
 			document.querySelector(`${clsName} input`).type = 'text';
 			document.querySelector(`${clsName} img`).src = 'app/assets/imgs/view-hide-svgrepo-com.svg';
 		}
+	}
+
+	removeField = (fieldName) => {
+		FormHelper.delField(this,this.formRef,fieldName);
+		FormHelper.delField(this,this.formRef,fieldName.replace('val','key'));
+		document.querySelectorAll(`.remove-dyn-field-${fieldName}`).forEach(itm => itm.remove());
+	};
+	
+
+	async createSecret(){
+		const validate = await this.formRef.validate();
+		console.log(`ERROR COUNT: `, this.formRef.errorCount);
+
+		if(validate){
+
+			dbConfig = {
+				'plugin_name': this.dbEngine.value,
+				'connection_url': 'postgresql://{{username}}:{{password}}@{{host}}:{{port}}/{{dbname}}',
+				'verify_connection': false,
+				'username': this.dbUser.value,
+				'password': this.firstValue.value,
+				'dbname': this.dbName.value,
+				'host': this.dbHost.value,
+				'port': this.dbPort.value,
+				'connectionName': this.connectionName.value
+			}
+			WorkspaceService.createSecret({ env: this.getDynamicFields(), dbConfig });
+			
+		}else{
+			AppTemplate.toast.error('Please fill all required field');
+		}
+		
 	}
 
 }
