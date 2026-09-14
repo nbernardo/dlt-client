f83fcd7f643b97f521dbf68aa89c46e57e98262a
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2026-09-13 20:57:57 -0400

    fixing the regex of set env ip

--- make/set-env.mk
+++ make/set-env.mk
@@ -1,12 +1,11 @@
 ENV ?= dev
 
 set-env:
-	@echo "######### Setting UI environment to '$(ENV)' in app-setup.js..."
-	@sed -i.bak -E "s#^[^|]*\|\|#    loadEnv = () => this.setConfigFile('$(ENV)') \|\|#" ./ui/config/app-setup.js && rm -f ./ui/config/app-setup.js.bak
-	@sed -i.bak -E 's|("baseUrl": "https://)[^:]*(:9443/api")|\1$(IP_ADDR)\2|g' ./ui/config/settings/stage.json && rm -f ./ui/config/settings/stage.json.bak
-	@sed -i.bak -E 's|("websocketAddr": "wss://)[^:]*(:9443/pipeline")|\1$(IP_ADDR)\2|g' ./ui/config/settings/stage.json && rm -f ./ui/config/settings/stage.json.bak
-	@sed -i.bak -E 's|("baseUrl": "https://)[^:]*(:443/api")|\1$(IP_ADDR)\2|g' ./ui/config/settings/prod.json && rm -f ./ui/config/settings/prod.json.bak
-	@sed -i.bak -E 's|("websocketAddr": "wss://)[^:]*(:443/pipeline")|\1$(IP_ADDR)\2|g' ./ui/config/settings/prod.json && rm -f ./ui/config/settings/prod.json.bak
+	@echo "######### Setting UI environment to '$(ENV)' and IP to '$(IP_ADDR)'..."
+	@sed -i.bak -E 's#("baseUrl": "https://)[^/]+(:9443/api")#\1$(IP_ADDR)\2#g' ./ui/config/settings/stage.json && rm -f ./ui/config/settings/stage.json.bak
+	@sed -i.bak -E 's#("websocketAddr": "wss://)[^/]+(:9443/pipeline")#\1$(IP_ADDR)\2#g' ./ui/config/settings/stage.json && rm -f ./ui/config/settings/stage.json.bak
+	@sed -i.bak -E 's#("baseUrl": "https://)[^/]+(:443/api")#\1$(IP_ADDR)\2#g' ./ui/config/settings/prod.json && rm -f ./ui/config/settings/prod.json.bak
+	@sed -i.bak -E 's#("websocketAddr": "wss://)[^/]+(:443/pipeline")#\1$(IP_ADDR)\2#g' ./ui/config/settings/prod.json && rm -f ./ui/config/settings/prod.json.bak
 
 
 set-dev: certs
