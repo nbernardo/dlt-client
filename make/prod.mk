4f673edb5ec9f4885bc1f90adfd02869c721e277
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2026-09-14 23:28:47 -0400

    - adding machine ip placeholder in the env - adding machine ip replacement instruction in the stage and Prod Makefile targets

--- make/prod.mk
+++ make/prod.mk
@@ -6,6 +6,7 @@ PROD_KEY_PATH  ?= /etc/letsencrypt/live/$(PROD_DOMAIN)/privkey.pem
 prod: certs
 	@$(MAKE) set-env ENV=prod
 	@echo "######### Compiling Production Configuration (Port: 443, Cert: $(PROD_CERT_PATH))..."
+	@sed -i.bak -E 's|{{prd_machine_ip}}|https://$(IP_ADDR):9443|g' ./backend/src/.env && rm -f ./backend/src/.env.bak
 	@sed -e 's|{{PORT}}|443|g' \
 	     -e 's|{{CERT}}|$(PROD_CERT_PATH)|g' \
 	     -e 's|{{KEY}}|$(PROD_KEY_PATH)|g' \
